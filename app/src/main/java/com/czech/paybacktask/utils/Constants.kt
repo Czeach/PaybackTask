30efd82bb8f166c6f1fd275d9351afa2b66cf84b
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2022-08-02 16:08:09 +0100

    hide api key

--- app/src/main/java/com/czech/paybacktask/utils/Constants.kt
+++ app/src/main/java/com/czech/paybacktask/utils/Constants.kt
@@ -1,6 +1,9 @@
 package com.czech.paybacktask.utils
 
+import com.czech.paybacktask.BuildConfig
+
 object Constants {
 
     const val BASE_URL = "https://pixabay.com/"
+    const val API_KEY = BuildConfig.API_KEY
 }
