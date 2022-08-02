e4d1939abaab90cf45e886ccb684ac80c0030249
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2022-08-02 14:32:17 +0100

    set up retrofit

---
+++ app/src/main/java/com/czech/paybacktask/di/NetworkModule.kt
@@ -0,0 +1,52 @@
+package com.czech.paybacktask.di
+
+import com.czech.paybacktask.utils.Constants
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideHttpLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+            level = HttpLoggingInterceptor.Level.HEADERS
+            level = HttpLoggingInterceptor.Level.BASIC
+        }
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(httpLoggingInterceptor: HttpLoggingInterceptor): OkHttpClient {
+        return OkHttpClient
+            .Builder()
+            .connectTimeout(500, TimeUnit.SECONDS)
+            .readTimeout(500, TimeUnit.SECONDS)
+            .writeTimeout(500, TimeUnit.SECONDS)
+            .addInterceptor(httpLoggingInterceptor)
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
+        return Retrofit
+            .Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(Constants.BASE_URL)
+            .client(okHttpClient)
+            .build()
+    }
+
+}
---
+++ app/src/main/java/com/czech/paybacktask/utils/Constants.kt
@@ -0,0 +1,6 @@
+package com.czech.paybacktask.utils
+
+object Constants {
+
+    const val BASE_URL = "https://pixabay.com/"
+}
