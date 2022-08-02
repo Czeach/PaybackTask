72d0c2f16ea7db42b51f938820261bfe5a61a057
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2022-08-02 14:04:56 +0100

    integrated hilt

--- app/src/main/java/com/czech/paybacktask/ui/main/MainActivity.kt
+++ app/src/main/java/com/czech/paybacktask/ui/main/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.czech.paybacktask
+package com.czech.paybacktask.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.czech.paybacktask.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
