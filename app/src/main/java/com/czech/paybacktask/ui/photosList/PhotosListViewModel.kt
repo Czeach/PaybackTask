111b2e3833b4e5848fa362f2cc4f2340f46fde6f
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2022-08-04 13:56:54 +0100

    photos list viewmodel done

--- app/src/main/java/com/czech/paybacktask/ui/photosList/PhotosListViewModel.kt
+++ app/src/main/java/com/czech/paybacktask/ui/photosList/PhotosListViewModel.kt
@@ -1,7 +1,38 @@
 package com.czech.paybacktask.ui.photosList
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.czech.paybacktask.data.network.repositories.PhotoRepository
+import com.czech.paybacktask.utils.states.PhotosListState
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class PhotosListViewModel : ViewModel() {
+@HiltViewModel
+class PhotosListViewModel @Inject constructor(
+    private val photoRepository: PhotoRepository
+) : ViewModel() {
+
+    val photosListState = MutableStateFlow<PhotosListState?>(null)
+
+    fun getPhotos(query: String) {
+        viewModelScope.launch {
+            photoRepository.getSearchResult(query).collect { it ->
+                when {
+                    it.isLoading -> {
+                        photosListState.value = PhotosListState.Loading
+                    }
+                    it.data == null -> {
+                        photosListState.value = PhotosListState.Error(message = it.message.toString())
+                    }
+                    else -> {
+                        it.data.let { photos ->
+                            photosListState.value = PhotosListState.Success(data = photos)
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
---
+++ app/src/main/java/com/czech/paybacktask/utils/states/PhotosListState.kt
@@ -0,0 +1,9 @@
+package com.czech.paybacktask.utils.states
+
+import com.czech.paybacktask.data.network.model.Result
+
+sealed class PhotosListState {
+    data class Success(val data: List<Result.Hit>?): PhotosListState()
+    data class Error(val message: String): PhotosListState()
+    object Loading: PhotosListState()
+}
